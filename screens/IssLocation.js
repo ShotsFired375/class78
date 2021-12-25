aa3b5d5dc573ba04108a689468588593832644d4
ShotsFired375 <68354767+ShotsFired375@users.noreply.github.com>
2021-12-24 20:11:16 -0600

    Add files via upload

---
+++ screens/IssLocation.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { Alert, Text, View, StyleSheet, Platform, SafeAreaView, Image, ImageBackground} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {axios} from 'axios';
+
+export default class IssLocationScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            location:{},
+        }
+    }
+    componentDidMount() {
+        this.getISSLocation()
+    }
+    getISSLocation=()=>{
+        axios
+        .get("https://api.wheretheiss.at/v1/satellites/25544")
+        .then(response=>{
+            this.setState({location:response.data})
+        })
+        .catch(error=>{
+            Alert.alert(error.message)
+        })
+    }
+    render() {
+        if (Object.keys(this.state.location).length === 0) {
+            return(
+                <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
+                    <Text>Loading...</Text>
+                </View>
+            )
+        } else {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.safearea}/>
+                <ImageBackground source={require("../assets/bg.png")} styles={styles.bgIMG}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>ISS Location!</Text>
+                </View>
+                <View style={styles.mapContainer}>
+                    <MapView style={styles.map} region={{
+                        latitude:this.state.location.latitude,
+                        longitude:this.state.location.longitude,
+                        latitudeDelta:100,
+                        longitudeDelta:100
+                    }}>
+                        <Marker coordinate={{latitude:this.state.location.latitude, longitude:this.state.location.longitude}}>
+                            <Image source={require("../assets/iss_icon.png")} style={{height:50, width:50}}/>
+                        </Marker>
+                    </MapView>
+                </View>
+                <View style={styles.infoContainer}>
+                    <Text style={styles.infoText}>Latitude:{this.state.location.latitude}</Text>
+                    <Text style={styles.infoText}>Longitude:{this.state.location.longitude}</Text>
+                    <Text style={styles.infoText}>Altitude (km):{this.state.location.altitude}</Text>
+                    <Text style={styles.infoText}>Velocity (km/h):{this.state.location.velocity}</Text>
+                </View>
+                </ImageBackground>
+            </View>
+        )
+    }
+}
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    safearea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    bgIMG: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    mapContainer: {
+        flex:0.6
+    },
+    map: {
+        width:"100%",
+        height:"100%"
+    },
+    infoContainer: {
+        borderColor:"black",
+        flex:0.2,
+        backgroundColor:"grey",
+        marginTop:-10,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        padding:30
+    },
+    infoText:{
+        color:"black",
+        fontWeight:"bold",
+        fontSizze:18
+    },
+});
